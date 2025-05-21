d499e44a89015e4e3300cd4c19331e72a9dd60b8
Mohit Dewani <mohitcap177@gmail.com>
2025-05-21 22:22:12 +0530

    Registration, login, logout implementation complete, next will be API focused

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///genproj.db"
@@ -8,7 +8,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'SECRET_KEY'
 db = SQLAlchemy(app)
 
-class User(db.Model, UserMixin):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20), nullable=False)
     username = db.Column(db.String(20), nullable=False, unique=True)
@@ -22,16 +22,53 @@ class User(db.Model, UserMixin):
 def index():
     return render_template("index.html")
 
-@app.route('/login')
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        name = request.form.get("name")
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        if User.query.filter_by(username=username).first():
+             flash("Username already taken", "danger")
+             return redirect(url_for("register"))
+        
+        hashed_password = generate_password_hash(password)
+        new_user = User(name=name, username=username, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Registered successfully. Please log in.", "success")
+        return redirect(url_for("login"))
+    
+    return render_template("register.html")
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            session["user_id"] = user.id
+            flash("Logged in successfully!", "success")
+            return redirect(url_for("generate"))
+        else:
+            flash("Invalid username and passsword", "danger")
+    
     return render_template("login.html")
 
-@app.route('/register')
-def register():
-    return render_template("register.html")
+@app.route('/logout')
+def logout():
+    session.pop("user_id", None)
+    flash("Logged out successfully.", "info")
+    return redirect(url_for("index"))
 
 @app.route('/generate')
 def generate():
+    if "user_id" not in session:
+        flash("Please log in to user the generator.", "warning")
+        return redirect(url_for("login"))
     return render_template("generate.html")
 
 if __name__ == "__main__":
